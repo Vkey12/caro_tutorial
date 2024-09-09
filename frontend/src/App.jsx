635209c06e8fbc97a25c70bca3f4c7b16200c0fd
victor <victor.odhi@gmail.com>
2024-09-09 09:49:47 +0300

    setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {Routes, Route} from 'react-router-dom'
+
+const App = () => {
+  return (
+    // padding for different screans small medium large
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] 1g:px-[9vw]">
+
+      <Routes>
+
+
+      </Routes>
+    </div>
+  )
+}
+
+export default App
