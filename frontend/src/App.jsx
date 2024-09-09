20950e917b1fb7d3ccc639254e086f9850e2458d
victor <victor.odhi@gmail.com>
2024-09-09 10:05:21 +0300

    created the routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Collection from "./pages/Collection"
+import Product from "./pages/Product"
+import Cart from "./pages/Cart"
+import Login from "./pages/Login"
+import Orders from "./pages/Orders"
+import PlaceOrder from "./pages/PlaceOrder"
 
 const App = () => {
   return (
@@ -7,7 +16,15 @@ const App = () => {
     <div className="px-4 sm:px-[5vw] md:px-[7vw] 1g:px-[9vw]">
 
       <Routes>
-
+        <Route path="/" element={<Home/>}/>
+        <Route path="/collection" element={<Collection/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/product/:productId" element={<Product/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/orders" element={<Orders/>}/>
+        <Route path="/place-order" element={<PlaceOrder/>}/>
 
       </Routes>
     </div>
